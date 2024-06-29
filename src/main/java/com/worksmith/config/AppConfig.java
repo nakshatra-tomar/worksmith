d2206edcd1c78cd7de9fcb819307dba2cbccc272
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-06-29 18:51:44 +0530

    Add spring security with JWT auth, cors

---
+++ src/main/java/com/worksmith/config/AppConfig.java
@@ -0,0 +1,64 @@
+package com.worksmith.config;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+
+        http.sessionManagement(Management -> Management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize->Authorize.requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf-> csrf.disable())
+                .cors(cors->cors.configurationSource(corsConfigurationSource()));
+//if any endpoint access starts with /api, it should be authenticated, without JWT token it should not be accessible
+        //sign up and login works without token
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+
+
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Arrays.asList( //has local ports which can access
+                        "https://localhost:3000",
+                        "https://localhost:5173",
+                        "https://localhost:4200" //for angular
+                ));
+                cfg.setAllowedMethods(Collections.singletonList("*"));//HTTP methods which are to be accessible, POST/GET, currently all allowed
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*")); // all header files allowed
+
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+
+                cfg.setMaxAge(3600L);
+
+
+                return cfg;
+
+
+            }
+        };
+
+
+    }
+
+}
