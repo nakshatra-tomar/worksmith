4e9857041ec94a2a42c1f079a42ea59a3c09de4e
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-06-29 17:17:46 +0530

    init

---
+++ src/main/java/com/worksmith/WorksmithApplication.java
@@ -0,0 +1,13 @@
+package com.worksmith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksmithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksmithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worksmith/WorksmithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worksmith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksmithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
