cde43c3664e2e50d0eac38f6ff142a25a37bcc15
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-08-13 00:36:49 +0530

    added invitation entity

---
+++ backend/src/main/java/com/worksmith/model/Invitation.java
@@ -0,0 +1,26 @@
+package com.worksmith.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Invitation {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+    private String email;
+    private Long projectId;
+
+}
